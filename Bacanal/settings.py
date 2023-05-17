317725e5f06cc8091fdda5d7a143f43c0ee69c98
Aldair Torres <86437315+Aldair-T@users.noreply.github.com>
2023-05-16 21:55:26 -0300

    Update settings.py

--- Bacanal/settings.py
+++ Bacanal/settings.py
@@ -93,20 +93,20 @@ WSGI_APPLICATION = 'Bacanal.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
-}"""
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
